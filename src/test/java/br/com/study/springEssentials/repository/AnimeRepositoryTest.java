38f2ad53aeb8ff505efee0b8364213a5dc84e704
ecoelho03 <ecoelho@contanciainvest.com.br>
2024-06-04 15:19:04 -0300

    corrigindo maven error

--- src/test/java/br/com/study/springEssentials/repository/AnimeRepositoryTest.java
+++ src/test/java/br/com/study/springEssentials/repository/AnimeRepositoryTest.java
@@ -7,10 +7,8 @@ import lombok.extern.log4j.Log4j2;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 import java.util.Optional;
